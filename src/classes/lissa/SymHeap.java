a803745bc5a35eb349a0300e215a3ac0dfb77781
JuanmaCopia <jmcopia96@gmail.com>
2022-11-30 12:33:26 +0100

    removed uninplemented methods from SymHeap class

--- src/classes/lissa/SymHeap.java
+++ src/classes/lissa/SymHeap.java
@@ -9,7 +9,7 @@ public class SymHeap {
         makeSymbolicImplicitInputThis(name, thisObject);
         return thisObject;
     }
-    
+
     public static void assume(boolean c) {
         if(!c)
             Verify.ignoreIf(true);
@@ -20,17 +20,17 @@ public class SymHeap {
     native public static int getMaxScope();
 
     native public static void countPath();
-    
-    
+
+
     native public static boolean usingDriverStrategy();
 
     native public static boolean usingIfRepOKStrategy();
-    
+
     native public static boolean usingLIHybridStrategy();
-    
+
     native public static boolean usingSymSolveBasedStrategy();
-    
-    
+
+
     // Methods to create symbolic values:
 
     native public static int makeSymbolicInteger(String name);
@@ -48,7 +48,7 @@ public class SymHeap {
     native public static char makeSymbolicChar(String name);
 
     native public static String makeSymbolicString(String name);
-    
+
     // this method should be used instead of the native one in
     // the no-string-models branch of jpf-core
     public static String makeSymbolicString(String name, int size){
@@ -58,12 +58,5 @@ public class SymHeap {
          }
     	return new String(str);
     }
-    
-    
-    native public static boolean assertPropertyWithSymSolve(String propertyMethodName, Object obj);
-    
-    native public static boolean assertPropertyWithSymSolveUsePropFinitization(String propertyMethodName, Object obj);
-    
-    native public static void printOutputVector(Object obj);
 
 }
