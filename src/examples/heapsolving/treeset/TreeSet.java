cfafe129069557dd9ed02356b42a5b771167e356
JuanmaCopia <jmcopia96@gmail.com>
2022-12-12 13:19:36 +0100

    unified treeset class and finitization

--- src/examples/heapsolving/treeset/TreeSet.java
+++ src/examples/heapsolving/treeset/TreeSet.java
@@ -7,6 +7,15 @@
 
 package heapsolving.treeset;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.SortedSet;
+
+import korat.finitization.IFinitization;
+import korat.finitization.IObjSet;
+import korat.finitization.impl.FinitizationFactory;
+
 /**
  * This class implements the <tt>Set</tt> interface, backed by a
  * <tt>TreeMap</tt> instance. This class guarantees that the sorted set will be
@@ -175,6 +184,21 @@ public class TreeSet {
         if (m == null)
             return false;
         return m.isBinTreeWithParentReferences();
+    }
+
+    public static IFinitization finTreeSet(int nodesNum) {
+        IFinitization f = FinitizationFactory.create(TreeSet.class);
+        IObjSet treemap = f.createObjSet(TreeMap.class, 1, true);
+        f.set(TreeSet.class, "m", treemap);
+        IObjSet nodes = f.createObjSet(TreeMap.Entry.class, nodesNum, true);
+        f.set(TreeMap.class, "root", nodes);
+        f.set(TreeMap.class, "size", f.createIntSet(0, nodesNum));
+        f.set(TreeMap.Entry.class, "key", f.createIntSet(0, nodesNum - 1));
+        f.set(TreeMap.Entry.class, "left", nodes);
+        f.set(TreeMap.Entry.class, "right", nodes);
+        f.set(TreeMap.Entry.class, "parent", nodes);
+        f.set(TreeMap.Entry.class, "color", f.createBooleanSet());
+        return f;
     }
 
 }
