a8a37fba2ffb6523a4c83983348fcc94c81407c5
JuanmaCopia <jmcopia96@gmail.com>
2022-12-13 13:54:06 +0100

    unified sqlfilterclasues class and finitization

--- src/examples/heapsolving/sqlfilterclauses/SQLFilterClauses.java
+++ src/examples/heapsolving/sqlfilterclauses/SQLFilterClauses.java
@@ -20,6 +20,10 @@ package heapsolving.sqlfilterclauses;
  */
 import java.io.Serializable;
 
+import korat.finitization.IFinitization;
+import korat.finitization.IObjSet;
+import korat.finitization.impl.FinitizationFactory;
+
 /**
  * This class contains the information used to build the Where and Order By
  * clauses used by the database query triggered by selecting the Contents tab in
@@ -89,6 +93,40 @@ public class SQLFilterClauses implements Serializable {
         }
 
         return true;
+    }
+
+    public static IFinitization finSQLFilterClauses(int nodesNum) {
+        IFinitization f = FinitizationFactory.create(SQLFilterClauses.class);
+
+        IObjSet hashmap = f.createObjSet(HashMapStrHmap.class, 1, true);
+        f.set(SQLFilterClauses.class, "_sqlClauseInformation", hashmap);
+
+        IObjSet entries = f.createObjSet(HashMapStrHmap.EntrySH.class, nodesNum, true);
+        f.set(HashMapStrHmap.class, "e0", entries);
+        f.set(HashMapStrHmap.class, "e1", entries);
+        f.set(HashMapStrHmap.class, "e2", entries);
+        f.set(HashMapStrHmap.class, "e3", entries);
+        f.set(HashMapStrHmap.class, "e4", entries);
+        f.set(HashMapStrHmap.class, "e5", entries);
+        f.set(HashMapStrHmap.class, "e6", entries);
+        f.set(HashMapStrHmap.class, "e7", entries);
+
+        IObjSet subhashmaps = f.createObjSet(HashMapStrStr.class, nodesNum, true);
+        f.set(HashMapStrHmap.EntrySH.class, "value", subhashmaps);
+        f.set(HashMapStrHmap.EntrySH.class, "next", entries);
+
+        IObjSet entries2 = f.createObjSet(HashMapStrStr.EntrySS.class, nodesNum, true);
+        f.set(HashMapStrStr.class, "e0", entries2);
+        f.set(HashMapStrStr.class, "e1", entries2);
+        f.set(HashMapStrStr.class, "e2", entries2);
+        f.set(HashMapStrStr.class, "e3", entries2);
+        f.set(HashMapStrStr.class, "e4", entries2);
+        f.set(HashMapStrStr.class, "e5", entries2);
+        f.set(HashMapStrStr.class, "e6", entries2);
+        f.set(HashMapStrStr.class, "e7", entries2);
+
+        f.set(HashMapStrStr.EntrySS.class, "next", entries2);
+        return f;
     }
 
 }
