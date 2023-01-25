9ce6587c0048516b5602c76193499c87defe8d47
JuanmaCopia <jmcopia96@gmail.com>
2023-01-25 09:55:27 +0100

    saved little example

--- src/examples/myexample/Example2.java
+++ src/examples/myexample/Example2.java
@@ -1,7 +1,19 @@
 package myexample;
 
+import gov.nasa.jpf.symbc.Debug;
+
 public class Example2 {
     public static void main(String[] args) {
+
+        int X = Debug.makeSymbolicInteger("X");
+        if (X == 0) {
+            if (X >= 0) {
+                // System.out.println("\n" + Debug.getSolvedPC());
+            } else {
+                // System.out.println("\n" + Debug.getSolvedPC());
+            }
+        }
+        System.out.println("\n" + Debug.getSolvedPC());
         System.out.println("\nExecuted!!!");
     }
 }
