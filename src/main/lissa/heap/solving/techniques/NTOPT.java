6ef8afc45b3d9a6b054491b61b486a122611e949
JuanmaCopia <jmcopia96@gmail.com>
2023-02-09 13:16:24 +0100

    added string path condition check in conjunction method of NTOPT

--- src/main/lissa/heap/solving/techniques/NTOPT.java
+++ src/main/lissa/heap/solving/techniques/NTOPT.java
@@ -3,11 +3,15 @@ package lissa.heap.solving.techniques;
 import gov.nasa.jpf.symbc.numeric.Comparator;
 import gov.nasa.jpf.symbc.numeric.Constraint;
 import gov.nasa.jpf.symbc.numeric.Expression;
+import gov.nasa.jpf.symbc.numeric.PCChoiceGenerator;
 import gov.nasa.jpf.symbc.numeric.PathCondition;
+import gov.nasa.jpf.symbc.string.StringComparator;
+import gov.nasa.jpf.symbc.string.StringConstraint;
+import gov.nasa.jpf.symbc.string.StringExpression;
+import gov.nasa.jpf.symbc.string.StringPathCondition;
 import gov.nasa.jpf.vm.Instruction;
 import gov.nasa.jpf.vm.ThreadInfo;
 import lissa.choicegenerators.HeapChoiceGeneratorLISSA;
-import gov.nasa.jpf.symbc.numeric.PCChoiceGenerator;
 import lissa.heap.SymHeapHelper;
 import lissa.heap.SymbolicInputHeapLISSA;
 import symsolve.vector.SymSolveSolution;
@@ -32,7 +36,7 @@ public class NTOPT extends NT {
         PathCondition currentProgramPC = pcCG.getCurrentPC();
         assert (currentProgramPC != null);
         PathCondition repOKPC = heapCG.getCurrentRepOKPathCondition();
-        if (isConjuntionSAT(currentProgramPC.make_copy(), repOKPC.make_copy())) {
+        if (isConjunctionSAT(currentProgramPC, repOKPC)) {
             primitiveBranchingCacheHits++;
             return nextInstruction;
         }
@@ -58,20 +62,48 @@ public class NTOPT extends NT {
                 heapCG, false);
     }
 
-    boolean isConjuntionSAT(PathCondition pc1, PathCondition pc2) {
-        PathCondition conjuntion = pc1.make_copy();
+    boolean isConjunctionSAT(PathCondition pc1, PathCondition pc2) {
+        assert (pc1 != null && pc2 != null);
+
+        PathCondition conjunction = pc1.make_copy();
 
         Constraint current = pc2.header;
         while (current != null) {
             Expression left = current.getLeft();
             Expression right = current.getRight();
             Comparator comp = current.getComparator();
-            conjuntion._addDet(comp, left, right);
+            conjunction._addDet(comp, left, right);
 
             current = current.and;
         }
 
-        return conjuntion.simplify();
+        if (!conjunction.simplify())
+            return false;
+
+        // check conjunctions of string path conditions
+        StringPathCondition spc1 = pc1.spc;
+        StringPathCondition spc2 = pc2.spc;
+
+        if (spc1 == null && spc2 == null)
+            return true;
+        if (spc1 == null)
+            return spc2.simplify();
+        if (spc2 == null)
+            return spc1.simplify();
+
+        StringPathCondition spc_conjunction = spc1.make_copy(pc1);
+
+        StringConstraint scurrent = spc2.header;
+        while (scurrent != null) {
+            StringExpression left = scurrent.getLeft();
+            StringExpression right = scurrent.getRight();
+            StringComparator comp = scurrent.getComparator();
+            spc_conjunction._addDet(comp, left, right);
+
+            scurrent = scurrent.and();
+        }
+
+        return spc_conjunction.simplify();
     }
 
 }
