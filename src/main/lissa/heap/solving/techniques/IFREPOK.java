a93543989d516eb6bb306068bf3a823deb172561
JuanmaCopia <jmcopia96@gmail.com>
2023-02-13 21:48:36 +0100

    fixed ifrepok

--- src/main/lissa/heap/solving/techniques/IFREPOK.java
+++ src/main/lissa/heap/solving/techniques/IFREPOK.java
@@ -1,5 +1,15 @@
 package lissa.heap.solving.techniques;
 
+import gov.nasa.jpf.vm.Instruction;
+import gov.nasa.jpf.vm.ThreadInfo;
+import lissa.choicegenerators.HeapChoiceGeneratorLISSA;
+
 public class IFREPOK extends LISSA {
+
+    @Override
+    public Instruction handleLazyInitializationStep(ThreadInfo ti, Instruction currentInstruction,
+            Instruction nextInstruction, HeapChoiceGeneratorLISSA heapCG) {
+        return nextInstruction;
+    }
 
 }
