5ecab4a1a7a87aba20b05172265f3dc74ce12ceb
JuanmaCopia <jmcopia96@gmail.com>
2023-03-03 14:26:06 +0100

    setted up scripts for symbolic execution experiments

---
+++ scripts/symbolic_execution_exp.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+bash "$SCRIPT_DIR/treemap.sh"
+bash "$SCRIPT_DIR/binomialheap.sh"
+bash "$SCRIPT_DIR/transportstats.sh"
+bash "$SCRIPT_DIR/hashmap.sh"
+bash "$SCRIPT_DIR/schedule.sh"
+bash "$SCRIPT_DIR/avltree.sh"
+bash "$SCRIPT_DIR/linkedlist.sh"
+bash "$SCRIPT_DIR/treeset.sh"
+
+
---
+++ scripts/treemap.sh
@@ -0,0 +1,43 @@
+#/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+TARGET_SCRIPT="${SCRIPT_DIR}/run_upto.sh"
+
+SRC_FOLDER="src/examples/heapsolving/treemap"
+PACKAGE="heapsolving.treemap"
+
+
+CLASS_NAME="TreeMap"
+echo "===============================  Class: $CLASS_NAME  ================================="
+MINSCOPE=1
+MAXSCOPE=50
+
+
+METHOD="remove"
+echo "-------- Method: $METHOD --------"
+bash $TARGET_SCRIPT $CLASS_NAME $METHOD NTOPT $MINSCOPE $MAXSCOPE $SRC_FOLDER $PACKAGE "false"
+bash $TARGET_SCRIPT $CLASS_NAME $METHOD LISSA $MINSCOPE $MAXSCOPE $SRC_FOLDER $PACKAGE "false"
+bash $TARGET_SCRIPT $CLASS_NAME $METHOD IFREPOK $MINSCOPE $MAXSCOPE $SRC_FOLDER $PACKAGE "false"
+bash $TARGET_SCRIPT $CLASS_NAME $METHOD REPOKSOLVER $MINSCOPE $MAXSCOPE $SRC_FOLDER $PACKAGE "false"
+bash $TARGET_SCRIPT $CLASS_NAME $METHOD NT $MINSCOPE $MAXSCOPE $SRC_FOLDER $PACKAGE "false"
+
+
+METHOD="put"
+echo "-------- Method: $METHOD --------"
+bash $TARGET_SCRIPT $CLASS_NAME $METHOD NTOPT $MINSCOPE $MAXSCOPE $SRC_FOLDER $PACKAGE "false"
+bash $TARGET_SCRIPT $CLASS_NAME $METHOD LISSA $MINSCOPE $MAXSCOPE $SRC_FOLDER $PACKAGE "false"
+bash $TARGET_SCRIPT $CLASS_NAME $METHOD IFREPOK $MINSCOPE $MAXSCOPE $SRC_FOLDER $PACKAGE "false"
+bash $TARGET_SCRIPT $CLASS_NAME $METHOD REPOKSOLVER $MINSCOPE $MAXSCOPE $SRC_FOLDER $PACKAGE "false"
+bash $TARGET_SCRIPT $CLASS_NAME $METHOD NT $MINSCOPE $MAXSCOPE $SRC_FOLDER $PACKAGE "false"
+
+
+# METHOD="containsKey"
+# echo "-------- Method: $METHOD --------"
+
+
+# METHOD="containsValue"
+# echo "-------- Method: $METHOD --------"
+
+
+# METHOD="get"
+# echo "-------- Method: $METHOD --------"
