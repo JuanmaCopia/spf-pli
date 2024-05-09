0034291a55a20109e1daf2c7093e20bac3e841f8
JuanmaCopia <jmcopia96@gmail.com>
2024-05-09 11:51:35 +0200

    scripts for experiments set

--- scripts/exp1.sh
+++ scripts/exp1.sh
@@ -2,4 +2,9 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-bash ${SCRIPT_DIR}/schedule.sh
+bash "$SCRIPT_DIR/treemap.sh"
+bash "$SCRIPT_DIR/binomialheap.sh"
+bash "$SCRIPT_DIR/schedule.sh"
+bash "$SCRIPT_DIR/hashmap.sh"
+bash "$SCRIPT_DIR/linkedlist.sh"
+bash "$SCRIPT_DIR/treeset.sh"
